e080ffd3d57ab71be1788f866735c6ca1042f1a3
dmachine7 <60094449+dmachine7@users.noreply.github.com>
2021-01-02 20:29:01 +0530

    refer

--- src/components/screens/RefEarn/RefInvite/RefInvite.jsx
+++ src/components/screens/RefEarn/RefInvite/RefInvite.jsx
@@ -24,39 +24,33 @@ function RefInvite() {
       },
     ];
     return (
-      <div>
-        <div className="container-fluid2">
-            <div className="login1">
-                <h1>Invite your friends to TechoKids and Win Cash Rewards & MacBook!</h1>
-            </div>
-
-            <div className="login3">
-              <div className="extra">
-                <h3>Login to generate your unique referral link</h3>
-                <Link to="/login"><button className="button button4">Login</button></Link>
-              </div>
-              <img className="image2" src={gift} alt="Image-2"/>
-            </div>
-
-        </div>
-
-        <div className="container-fluid3">
-            <div className="about">
-          <h1>How it works</h1>
-          <div className="skills-grid">
-          {works.map((work) => (
-            <div className="skill-row">
-                      <img className="image3" src={work.img} alt="Image-3"/>
-                      <div className="info">
-                        <h2>{work.title}</h2>
-                        <p>{work.desc}</p>
-                      </div>
-                  </div>
-          ))}
+      <>
+        <div className="content-info" style={{display: "flex", justifyContent: "space-between", alignItems: "flex-end", flexWrap: "wrap"}}>
+          <div id="invite-login-div">
+            <h1>Invite your friends to TechoKids and <br />Win Cash Rewards & MacBook!</h1>
+            <p style={{fontSize: "1.25em", marginBottom: "2em"}}>Login to generate your unique referral link</p>
+            <Link to="/login"><button id="login">Login</button></Link>
           </div>
+          <img id ="gift" src={gift} style={{float: "right"}} width="auto" height="200px" style={{margin: "1em"}} />
         </div>
-      </div>
-      </div>
+        <div className="content-info">
+          <h1>How it works?</h1>
+          {
+            works.map( (item, index) => {
+              return (
+                <div className="works-list">
+                  <img src={item.img} style={{width: "150px", marginRight: "2em"}} />
+                  <div>
+                    <h2> {item.title} </h2>
+                    <p style={{fontSize: "22px"}}> {item.desc} </p>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </>
+      
     );
 }
 
